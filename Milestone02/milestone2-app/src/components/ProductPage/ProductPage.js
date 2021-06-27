988709e0e335319512954d89cfd046b2d51bbe72
jpsecundino <jp.secundino@usp.br>
2021-06-27 14:53:31 -0300

    created product page

---
+++ Milestone02/milestone2-app/src/components/ProductPage/ProductPage.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PresentationBanner from "../HomePage/PresentationBanner"
+
+function ProductPage() {
+    return (
+        <div>
+            <PresentationBanner />
+            
+        </div>
+    )
+}
+
+export default ProductPage
