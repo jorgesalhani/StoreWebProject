025a741313d7862a2b62b2d267002f0825ce89d4
jorgesalhani <jorge.salhani@usp.br>
2021-06-28 08:37:58 -0300

    Try to update db at RegisterPage. NOT WORKING

--- Milestone02/milestone2-app/src/components/RegisterPage/RegisterPage.js
+++ Milestone02/milestone2-app/src/components/RegisterPage/RegisterPage.js
@@ -8,26 +8,54 @@ function RegisterPage(props) {
             <div className={RegisterCardCSS.registerCard}>
                 <div className={RegisterCardCSS.infoBlock}>
                     <p>Nome</p>
-                    <input type="text" />
+                    <input className="NAME" type="text" />
                     <p>E-mail</p>
-                    <input type="text" />
+                    <input className="EMAIL" type="text" />
                     <p>Cartão cadastrado</p>
-                    <input type="text" />
+                    <input className="CCARD" type="text" />
                 </div>
                 <div className={RegisterCardCSS.infoBlock}>
                     <p>Usuário</p>
-                    <input type="text" />
+                    <input className="USER" type="text" />
                     <p>Endereço</p>
-                    <input type="text" />
+                    <input className="ADDRESS" type="text" />
                     <p>Data de nascimento</p>
-                    <input type="text" />
+                    <input className="BDAY" type="text" />
                 </div>
             </div>
-            <button className={RegisterCardCSS.editButton}>
+            
+            <button className={RegisterCardCSS.editButton}
+                    onClick={() => {
+                        RegisterProfile(
+                            document.getElementsByClassName("NAME")[0].value,
+                            document.getElementsByClassName("EMAIL")[0].value,
+                            document.getElementsByClassName("CCARD")[0].value,
+                            document.getElementsByClassName("USER")[0].value,
+                            document.getElementsByClassName("ADDRESS")[0].value,
+                            document.getElementsByClassName("BDAY")[0].value
+                            )
+                        }}>
                 Registrar
             </button>
         </>
     )
 }
+
+function RegisterProfile(name_, email_, ccard_, user_, add_, bday_) {
+    let jsonFileDB = require("../../database/db.json")
+    let formFills = {
+        name: name_,
+        email: email_,
+        ccard: ccard_,
+        user: user_,
+        add: add_,
+        bday: bday_
+    }
+    console.log(formFills)
+
+    // let stringJson = JSON.stringify(formFills)
+    // jsonFileDB.usersLogs.writeFile(stringJson)
+}
+
 
 export default RegisterPage
