5dcec72465cefcc3f6fcbe896ee43bea0e076031
jorgesalhani <jorge.salhani@usp.br>
2021-07-22 20:29:37 -0300

    fix order items

--- Milestone02/milestone2-app/src/components/ProfilePage/OrderCard.js
+++ Milestone02/milestone2-app/src/components/ProfilePage/OrderCard.js
@@ -6,15 +6,42 @@ import OrderCardCSS from "./OrderCard.module.css";
 function OrderCard(props) {
   return (
     <div className={OrderCardCSS.cartItem}>
-      <img className={OrderCardCSS.cartImageItem}
-           src={props.product.img} />
-      <div className={OrderCardCSS.cartInfoItem}>
-        <h3>{props.product.name}</h3>
-        <p>{props.product.description}</p>
-        <div className={OrderCardCSS.cartPriceAndButton}>
-          <p>Preço: {props.product.price}</p>
-        </div>
-      </div>
+      <table style={{ tableLayout:"fixed",
+                      borderCollapse: "collapse",
+                      margin: "5%"}} >
+        <tbody>
+          <tr>
+              <td style={{
+                          alignContent: "center",
+                          borderRadius: "5px",
+                          textAlign: "center",
+                          height: "100%",
+                          width: "40%"
+                        }}>
+                <img className={OrderCardCSS.cartImageItem}
+                    src={props.product.img} />
+              </td>
+              <div style={{
+                            alignContent: "center",
+                            borderRadius: "5px",
+                            boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+                            margin: "50px",
+                            padding: "5px",
+                            textAlign: "center",
+                            heigh: "100px",
+                            width: ""
+                            }}>
+                <div className={OrderCardCSS.cartInfoItem}>
+                  <h3>{props.product.name}</h3>
+                  <p>{props.product.description}</p>
+                  <div className={OrderCardCSS.cartPriceAndButton}>
+                    <p>Preço: R$ {props.product.price}</p>
+                  </div>
+                </div>
+              </div>
+            </tr>
+         </tbody> 
+      </table>
     </div>
   );
 }
